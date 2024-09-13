1454919e5be8514c7de8f3927aee9b94f382e224
Deniz Doğru <deniz.dogru@venhancer.com>
2024-09-13 10:28:13 +0300

    UserController service is implemented. CustomException class is created

---
+++ HotelManagement.API/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using HotelManagement.Core.Dtos;
+using HotelManagement.Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SharedLibrary.Exceptions;
+
+namespace HotelManagement.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : CustomBaseController
+{
+    private readonly IUserService _userService;
+
+    public UsersController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateUser(CreateUserDto createUserDto)
+    {
+        throw new CustomException("An error occurred related to the database.");
+    }
+
+    [Authorize]
+    [HttpGet]
+
+    public async Task<IActionResult> GetUser()
+    {
+        return ActionResultInstance(await _userService.GetUserByNameAsync(HttpContext.User.Identity.Name));
+    }
+}
