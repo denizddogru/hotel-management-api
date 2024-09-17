145193a2da35b281265a4f750cb2af7670fdd373
Deniz Doğru <deniz.dogru@venhancer.com>
2024-09-17 17:55:31 +0300

    Amenities field removed (hotel.cs)

--- HotelManagement.Core/Entity/Hotel.cs
+++ HotelManagement.Core/Entity/Hotel.cs
@@ -6,7 +6,6 @@ public class Hotel
     public string Address { get; set; }
     public string Description { get; set; }
     public string StarRating { get; set; }
-    public ICollection<string> Amenities { get; set; }
     public List<Room> Rooms { get; set; }
 
 
---
+++ HotelManagement.Data/Migrations/20240917145425_Amenities_Removed.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelManagement.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Amenities_Removed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Amenities",
+                table: "Hotel");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Amenities",
+                table: "Hotel",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
